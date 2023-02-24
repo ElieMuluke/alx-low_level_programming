7d19cdaeb2f3b4138b8afb47d269de0f1939d0d9
Elie Muluke <eliemuluke@gmail.com>
2023-02-24 09:02:27 -0800

    update: fix 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,21 @@
+#include "stdio.h"
 #include "main.h"
-
 /**
- * print_line - a function that draws a straight line in the terminal
- * @n: input number of times to print '_'
- * Return: a straight line
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * _putchar only 3 times
+ * Return: 0-14 x10 followed by new line
  */
 void print_line(int n)
 {
-    if (n <= 0) {
+    if (n <= 0)
+    {
         _putchar('\n');
         return;
     }
 
     int i;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         _putchar('_');
     }
     _putchar('\n');
