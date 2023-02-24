733b39cf9320eac4ae93fee9bc995628d5f0f2f0
Elie Muluke <eliemuluke@gmail.com>
2023-02-24 04:16:47 -0800

    update: 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,8 +6,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
 
-	return (0);
+int main(void) {
+    int sum = 0;
+    for (int i = 1; i < 1024; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
 }
