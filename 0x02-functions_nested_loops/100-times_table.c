813be5635ce0b2ba3fde0b794e2a5acbe6d1bc04
Elie Muluke <eliemuluke@gmail.com>
2023-02-24 03:33:41 -0800

    fix: error in 100-times_table.co

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,20 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-print_times_table(int n)
+void print_times_table(int n)
 {
-
-	return (n);
+    if (n < 0 || n > 15) {
+        return;
+    }
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            int res = i * j;
+            if (j == 0) {
+                printf("%d", res);
+            } else {
+                printf(", %d", res);
+            }
+        }
+        printf("\n");
+    }
 }
