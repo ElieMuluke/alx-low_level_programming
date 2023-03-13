5a188c7cd295c27c1b50f4898fc5b94705b35bf1
Elie Muluke <eliemuluke@gmail.com>
2023-03-13 10:41:40 +0300

    update: update file project name

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 void _puts_recursion(char *s){
-    if (*s == '\0'){ // base case - check if the string is empty
+    if (*s == '\0'){
         _putchar('\n');
         return;
     }
     _putchar(*s);
-    // _puts_recursion(s + 1);
+    _puts_recursion(s + 1);
 }
