b0ba31ccee625c08ef9151f6e27b516f3569f980
Elie Muluke <eliemuluke@gmail.com>
2023-03-13 10:47:02 +0300

    update: update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,15 @@
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s){
-    if (*s == '\0'){
+    if (*s == '\0')
+    {
         _putchar('\n');
-        return;
+    }else
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
     }
-    _putchar(*s);
-    _puts_recursion(s + 1);
 }
