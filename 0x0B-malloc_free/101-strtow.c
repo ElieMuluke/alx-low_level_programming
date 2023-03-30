a22e195f316e4bbdf97f60c12de1611b8d719009
Elie Muluke <eliemuluke@gmail.com>
2023-03-30 08:20:37 -0700

    update: 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * count_words - Counts the number of words in a string
@@ -8,23 +9,23 @@
  */
 int count_words(char *str)
 {
-        int i, in_word, count;
+    int i, in_word, count;
 
-        in_word = 0;
-        count = 0;
+    in_word = 0;
+    count = 0;
 
-        for (i = 0; str[i] != '\0'; i++) {
-                if (str[i] != ' ') {
-                        if (!in_word) {
-                                in_word = 1;
-                                count++;
-                        }
-                } else {
-                        in_word = 0;
-                }
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!in_word) {
+                in_word = 1;
+                count++;
+            }
+        } else {
+            in_word = 0;
         }
+    }
 
-        return count;
+    return count;
 }
 
 /**
@@ -35,66 +36,63 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-        char **words;
-        int i, j, k, n_words, word_len, in_word;
+    char **words;
+    int i, j, k, n_words, word_len, in_word;
 
-        if (str == NULL || str[0] == '\0') {
-                return NULL;
-        }
+    if (str == NULL || str[0] == '\0') {
+        return NULL;
+    }
 
-        n_words = count_words(str);
+    n_words = count_words(str);
 
-        if (n_words == 0) {
-                return NULL;
-        }
+    if (n_words == 0) {
+        return NULL;
+    }
 
-        words = malloc((n_words + 1) * sizeof(char *));
-        if (words == NULL) {
-                return NULL;
-        }
+    words = malloc((n_words + 1) * sizeof(char *));
+    if (words == NULL) {
+        return NULL;
+    }
 
-        in_word = 0;
-        word_len = 0;
-        j = 0;
+    in_word = 0;
+    word_len = 0;
+    j = 0;
 
-        for (i = 0; str[i] != '\0'; i++) {
-                if (str[i] != ' ') {
-                        if (!in_word) {
-                                in_word = 1;
-                                word_len = 0;
-                                k = i;
-                        }
-                        word_len++;
-                } else {
-                        if (in_word) {
-                                in_word = 0;
-                                words[j] = malloc((word_len + 1) * sizeof(char));
-                                if (words[j] == NULL) {
-                                        return NULL;
-                                }
-                                for (k = k; k < i; k++) {
-                                        words[j][k - k] = str[k];
-                                }
-                                words[j][k - k] = '\0';
-                                j++;
-                        }
-                }
-        }
-
-        if (in_word) {
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!in_word) {
+                in_word = 1;
+                word_len = 1;
+                k = i;
+            } else {
+                word_len++;
+            }
+        } else {
+            if (in_word) {
+                in_word = 0;
                 words[j] = malloc((word_len + 1) * sizeof(char));
                 if (words[j] == NULL) {
-                        return NULL;
+                    return NULL;
                 }
-                for (k = k; k < i; k++) {
-                        words[j][k - k] = str[k];
-                }
-                words[j][k - k] = '\0';
+                strncpy(words[j], &str[k], word_len);
+                words[j][word_len] = '\0';
                 j++;
+            }
         }
+    }
 
-        words[j] = NULL;
+    if (in_word) {
+        words[j] = malloc((word_len + 1) * sizeof(char));
+        if (words[j] == NULL) {
+            return NULL;
+        }
+        strncpy(words[j], &str[k], word_len);
+        words[j][word_len] = '\0';
+        j++;
+    }
 
-        return words;
+    words[j] = NULL;
+
+    return words;
 }
 
