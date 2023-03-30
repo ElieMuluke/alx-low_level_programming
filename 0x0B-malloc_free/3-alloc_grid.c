386a72f4f2efe98c1eb98de20c48546e65ee582c
Elie Muluke <eliemuluke@gmail.com>
2023-03-30 07:55:21 -0700

    update: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,19 +23,22 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		iarray[i] = malloc(sizeof(int) * width);
+
 		if (iarray[i] == NULL)
 		{
-			for (;i>=0; i--)
+			for (; i >= 0; i--)
 				free(iarray[i]);
+
 			free(iarray);
 			return (NULL);
 		}
 	}
 
-	for(i=0; i<height; i++)
+	for(i = 0; i < height; i++)
 	{
-		for(n=0; n<width; n++)
-			iarray[i][n]=0;
+		for(n = 0; n < width; n++)
+			iarray[i][n] = 0;
 	}
+
 	return (iarray);
 }
