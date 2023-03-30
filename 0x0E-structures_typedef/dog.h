988e55aaaf3bc7b397f7fe82c2a4c18d3e4904f0
Elie Muluke <eliemuluke@gmail.com>
2023-03-30 15:57:11 -0700

    update: dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,31 +5,32 @@
 #include <stdlib.h>
 
 /**
-*struct dog - a new type of data
-*representing a dog
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
-*/
+ * struct dog - a new type of data representing a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's name
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
-
 /**
-*dog_t - Typedef for struct dog
-*/
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
-
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+/* Helper function prototypes */
 int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
+
