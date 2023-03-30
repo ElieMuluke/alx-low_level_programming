cdc5fa5096b7b9f5a236cfb4df9d7c1581379577
Elie Muluke <eliemuluke@gmail.com>
2023-03-30 16:54:22 -0700

    update: 1-print_numbers.c 2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
 /**
  * print_numbers - Entry Point
  * @separator: comma space
@@ -9,22 +10,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
+    va_list valist;
+    unsigned int i;
 
-	if (separator == NULL)
-		return;
+    va_start(valist, n);
 
-	va_start(valist, n);
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(valist, const unsigned int));
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", va_arg(valist, const unsigned int));
+        if (separator != NULL && i != (n - 1))
+            printf("%s", separator);
+    }
+    printf("\n");
 
-		if (i != (n - 1))
-			printf("%s", separator);
-	}
-	printf("\n");
-
-	va_end(valist);
+    va_end(valist);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,37 +1,35 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
 /**
- * print_strings - Entry Point
- * @separator: comma space
- * @n: number of elements
- * Return: void
+ * print_strings - prints strings
+ * @separator: separator to print between strings
+ * @n: number of strings to print
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	char *str;
+        va_list args;
+        unsigned int i;
+        char *str;
 
-	if (separator == NULL)
-		return;
+        va_start(args, n);
 
-	va_start(valist, n);
+        for (i = 0; i < n; i++)
+        {
+                str = va_arg(args, char *);
 
-	for (i = 0; i < n; i++)
-	{
-		str = va_arg(valist, char *);
-		if (str == NULL)
-		{
-			str = "(nil)";
-		}
-		printf("%s", str);
-		if (i != (n - 1))
-		{
-			printf("%s", separator);
-		}
-	}
-	printf("\n");
+                if (str == NULL)
+                        printf("(nil)");
+                else
+                        printf("%s", str);
 
-	va_end(valist);
+                if (separator != NULL && i != n - 1)
+                        printf("%s", separator);
+        }
+
+        printf("\n");
+
+        va_end(args);
 }
+
