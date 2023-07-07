5d7478b6a817bb7325b52e9b0cdddbe97bd65a87
Elie Muluke <61221932+ElieMuluke@users.noreply.github.com>
2023-07-07 02:15:20 +0000

    update: implement 5-flip_bit.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - To count the number of bits to be changed
+ * so that we can get from one number to another
+ * @x: is the first number
+ * @y: is the second number
+ *
+ * Return: the number of bits to be changed
+ */
+unsigned int flip_bits(unsigned long int x, unsigned long int y)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = x ^ y;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
+
